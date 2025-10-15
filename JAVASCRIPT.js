867be9277f4ece9bbe4eecfd8392c2865c5d1938
kshyxxz <kshyxxz@gmail.com>
2025-10-15 20:21:34 +0545

    learning JAVASCRIPT

---
+++ JAVASCRIPT.js
@@ -0,0 +1,11 @@
+// var removeCartItemButtons = document.getElementsByClassName("btn-danger")
+// console.log(removeCartItemButtons)
+// for (var i = 0; i < removeCartItemButtons.length; i++) {
+//     var button = removeCartItemButtons[i]
+//     button.addEventListener("click", function(event) }
+// )
+
+
+    // normal
+    // async
+    // defer
