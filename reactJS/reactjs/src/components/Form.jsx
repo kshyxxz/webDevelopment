f0762e76858db2188ea5fee255c6cdcc42de2602
kshyxxz <kshyxxz@gmail.com>
2025-11-28 20:23:23 +0545

    APIcallOnReact + PrettierFix

--- reactJS/reactjs/src/components/Form.jsx
+++ reactJS/reactjs/src/components/Form.jsx
@@ -1,19 +1,39 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function Form() {
-    const [name, setName] = useState({firstName: "", middleName: "", lastName: ""});
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(name);
-    }
-    return (
-        <>
-            <form>
-                <input type="text" value={name.firstName} onChange={(e) => setName({...name, firstName: e.target.value})}/>
-                <input type="text" value={name.middleName} onChange={(e) => setName({...name, middleName: e.target.value})}/>
-                <input type="text" value={name.lastName} onChange={(e) => setName({...name, lastName: e.target.value})}/>
-                <button onClick={(e) => handleSubmit(e)}>Submit</button>
-            </form>                                             {/* ... spread operator for concat*/}
-        </>
-    );
+	const [name, setName] = useState({ firstName: '', middleName: '', lastName: '' });
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(name);
+	};
+	const URL = 'https://restcountries.com/v3.1/name/nepal';
+	async function countryDetails() {
+		let response = await fetch(URL);
+		let jsonResponse = await response.json();
+		console.log(jsonResponse[0].altSpellings[2]);
+	}
+	return (
+		<>
+			<form>
+				<input
+					type="text"
+					value={name.firstName}
+					onChange={(e) => setName({ ...name, firstName: e.target.value })}
+				/>
+				<input
+					type="text"
+					value={name.middleName}
+					onChange={(e) => setName({ ...name, middleName: e.target.value })}
+				/>
+				<input
+					type="text"
+					value={name.lastName}
+					onChange={(e) => setName({ ...name, lastName: e.target.value })}
+				/>
+				<button onClick={(e) => handleSubmit(e)}>Submit</button>
+			</form>
+			{/* ... spread operator for concat*/}
+			<button onClick={countryDetails}>Nepal</button>
+		</>
+	);
 }
