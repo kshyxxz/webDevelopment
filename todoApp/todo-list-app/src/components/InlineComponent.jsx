0807adbacc1b7d6d9672fb6780b12b62c0708e9f
kshyxxz <kshyxxz@gmail.com>
2025-12-03 19:10:49 +0545

    reactMiss5

--- todoApp/todo-list-app/src/components/InlineComponent.jsx
+++ todoApp/todo-list-app/src/components/InlineComponent.jsx
@@ -1,7 +1,7 @@
 export default function InlineComponent() {
 	return (
 		<>
-			<h1 style={{ color: 'blue', bgcolor: 'teal' }}>Inline Component</h1>
+			<h1 style={{ color: 'blue', backgroundColor: 'red' }}>Inline Component</h1>
 		</>
 	);
 }
